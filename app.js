5f782ae85ffe949a2cd1fe48834c15bace189c04
tanvir-ux <tanviralamshuvo@gmail.com>
2022-06-27 00:59:08 +0600

    Display computer choice based on random number

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ const resultDisplay = document.getElementById("result")
 
 const possibleChoices = document.querySelectorAll("button")
 let userChoice
+let computerChoice
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {    
     userChoice = e.target.id 
     userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
 }))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1
+    if (randomNumber === 1) computerChoice = "rock"
+    if (randomNumber === 2) computerChoice = "paper"
+    if (randomNumber === 3) computerChoice = "scissor"
+    computerChoiceDisplay.innerHTML = computerChoice   
+}
+
